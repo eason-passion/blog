3956ac4de6a173a7ff3d03330f81354b4f093fe3
Eason <1371356187@qq.com>
2025-03-02 20:21:16 +0800

    SwaggerConfig 配置类

---
+++ src/main/java/com/yhh/config/SwaggerConfig.java
@@ -0,0 +1,15 @@
+package com.yhh.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author Eason
+ * @Date 2025-03-02 20:17
+ * @PackageName:com.yhh.config
+ * @ClassName: SwaggerConfig
+ * @Description: TODO
+ */
+@Configuration
+public class SwaggerConfig {
+    //配置类
+}
