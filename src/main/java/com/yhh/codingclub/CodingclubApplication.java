f8b9fc1c68f2a656cb49d2c9f4a581f42598cb19
Eason <1371356187@qq.com>
2025-03-02 19:48:19 +0800

    init 初始化

---
+++ src/main/java/com/yhh/codingclub/CodingclubApplication.java
@@ -0,0 +1,13 @@
+package com.yhh.codingclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingclubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingclubApplication.class, args);
+    }
+
+}
